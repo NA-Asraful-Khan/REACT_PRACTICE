d1879ce3d36437cf24f2d071b8038e00c4c9bcb9
NA-Asraful-Khan <soyonshikhor@gmail.com>
2024-05-22 14:09:49 +0600

    Made some correction on Basic Table

--- react-table/src/componants/BasicTable.jsx
+++ react-table/src/componants/BasicTable.jsx
@@ -6,12 +6,6 @@ export const BasicTable = () => {
 
     const columns = useMemo(() => COLUMNS, [])
     const data = useMemo(() => MOCK_DATA, [])
-    console.log(data)
-
-    const tableInstance = useTable({
-        columns: columns,
-        data: data
-    })
 
     const {
         getTableProps,
@@ -19,27 +13,30 @@ export const BasicTable = () => {
         headerGroups,
         rows,
         prepareRow
-    } = tableInstance
+    } = useTable({
+        columns,
+        data}) 
+
+        
     return (
         <table {...getTableProps()}>
             <thead>
-                {headerGroups.map((headerGroup,index) => (
-                    <tr key={index-1} {...headerGroup.getHeaderGroupProps()}>
-                        {headerGroup.headers.map((column,index) => (
-                            <th key={index+1} {...column.getHeaderProps()}>{column.render('Header')}</th>
+                {headerGroups.map((headerGroup) => (
+                    <tr {...headerGroup.getHeaderGroupProps()}>
+                        {headerGroup.headers.map((column) => (
+                            <th {...column.getHeaderProps()}>{column.render('Header')}</th>
                         ))}
                     </tr>
                 ))}
             </thead>
             <tbody {...getTableBodyProps()}>
                 {
-                    rows.map((row,index)=>{
+                    rows.map((row)=>{
                         prepareRow(row)
                         return(
-                            <tr key={index} {...row.getRowProps()}>
-                                {
-                                    row.cells.map((cell,index) =>{
-                                        <td key={index} {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                            <tr {...row.getRowProps()}>
+                                {row.cells.map((cell) =>{
+                                       return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
                                     })
                                 }
                             </tr>
