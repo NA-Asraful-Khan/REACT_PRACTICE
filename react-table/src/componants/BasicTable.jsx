be5b4dea5aa28a7f2d72e8f5e09ddee58bef4b0f
NA-Asraful-Khan <soyonshikhor@gmail.com>
2024-05-22 14:31:17 +0600

    create group column

--- react-table/src/componants/BasicTable.jsx
+++ react-table/src/componants/BasicTable.jsx
@@ -1,10 +1,10 @@
 import React, { useMemo } from 'react'
 import { useTable } from 'react-table'
 import MOCK_DATA from './MOCK_DATA.json'
-import { COLUMNS } from './columns'
+import { COLUMNS , GROUPED_COLUMNS} from './columns'
 export const BasicTable = () => {
 
-    const columns = useMemo(() => COLUMNS, [])
+    const columns = useMemo(() => GROUPED_COLUMNS, [])
     const data = useMemo(() => MOCK_DATA, [])
 
     const {
@@ -47,18 +47,13 @@ export const BasicTable = () => {
                 }
             </tbody>
             <tfoot>
-                {
-                    footerGroups.map((footerGroup) => (
-                        <tr {...footerGroup.getFooterGroupProps}>
-                            {
-                                footerGroup.headers.map((column) => (
-                                    <td {...column.getFooterProps}>
-                                        {
-                                            column.render('Footer')
-                                        }
+                { footerGroups.map((footerGroup) => (
+                        <tr {...footerGroup.getFooterGroupProps()}>
+                            {footerGroup.headers.map((column) => (
+                                    <td {...column.getFooterProps()}>
+                                        {column.render('Footer')}
                                     </td>
-                                ))
-                            }
+                                ))}
                         </tr>
                     ))
                 }
