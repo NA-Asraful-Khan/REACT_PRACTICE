5bc10a469816635dc8709e1557b5c4668d869c04
NA-Asraful-Khan <soyonshikhor@gmail.com>
2024-05-22 16:53:24 +0600

    set time delay on global filter

--- react-table/src/componants/GlobalFilter.jsx
+++ react-table/src/componants/GlobalFilter.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const GlobalFilter = ({filter,setFilter}) => {
+  const [value, setValue]=useState()
+
+  setTimeout(() => {
+    // Set the new value after the delay
+    setFilter(value);
+  }, 700);
   return (
     <span>
         Search:{' '}
-        <input value={filter} onChange={e=>setFilter(e.target.value)} />
+        <input value={value} onChange={e=>{
+          setValue(e.target.value)
+        }} />
     </span>
   )
 }
