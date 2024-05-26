7ff09031f433de108d22eadb6fe0738978d51dfa
NA-Asraful-Khan <soyonshikhor@gmail.com>
2024-05-26 09:24:51 +0600

    create custom hook

--- react-query/src/components/RQSuperHeroes.page.jsx
+++ react-query/src/components/RQSuperHeroes.page.jsx
@@ -1,10 +1,7 @@
-import axios from "axios"
-import { useQuery } from "react-query"
+
+import { useSuperHerosData } from "./useSuperHerosData"
 
 
-const fetchSuperHeros=()=>{
-  return axios.get('http://localhost:4000/superheroes')
-}
 
 export const RQSuperHeroesPage = () => {
 
@@ -15,26 +12,7 @@ export const RQSuperHeroesPage = () => {
   const onError = (e)=>{
     console.log('Perform side effect after an error',e)
   }
-  const {isLoading,data,isError,error,refetch}= useQuery(
-    'super-heros',
-    fetchSuperHeros,
-    {
-      // staleTime:30000,
-      // cacheTime:5000,
-      // refetchOnMount:true,
-      refetchOnWindowFocus:true,
-      refetchInterval: 15000,
-      refetchIntervalInBackground:true,
-      // enabled:false
-
-      onError,
-      onSuccess,
-      select:(data)=>{
-        const superHeroNames = data.data.map((hero)=> hero.name)
-        return superHeroNames
-      }
-    }
-  )
+  const {isLoading,data,isError,error,refetch}= useSuperHerosData(onError,onSuccess)
 
   if(isLoading){
     return <h2>Loading ....</h2>
---
+++ react-query/src/components/useSuperHerosData.js
@@ -0,0 +1,32 @@
+
+import axios from "axios"
+import { useQuery } from "react-query"
+
+
+const fetchSuperHeros=()=>{
+  return axios.get('http://localhost:4000/superheroes')
+}
+
+export const useSuperHerosData=(onError,onSuccess)=>{
+    return useQuery(
+        'super-heros',
+        fetchSuperHeros,
+        {
+          // staleTime:30000,
+          // cacheTime:5000,
+          // refetchOnMount:true,
+          refetchOnWindowFocus:true,
+          refetchInterval: 15000,
+          refetchIntervalInBackground:true,
+          // enabled:false
+    
+          onError,
+          onSuccess,
+          select:(data)=>{
+            const superHeroNames = data.data.map((hero)=> hero.name)
+            return superHeroNames
+          }
+        }
+      )
+    
+}
