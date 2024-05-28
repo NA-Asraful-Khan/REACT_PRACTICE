487a88478bfb11feac65e479178fb33f17349061
NA-Asraful-Khan <soyonshikhor@gmail.com>
2024-05-28 12:25:22 +0600

    TS Repush

--- react-typescript/src/componants/Greet.tsx
+++ react-typescript/src/componants/Greet.tsx
@@ -9,7 +9,7 @@ export const Greet = ({name,messageCount,isLoggedIn}:GreetProps) => {
   return (
     <>
     {
-        isLoggedIn?<h2>Hello {name}! You have {messageCount?messageCount:0} message unread.</h2>: <h2>Hello User! Please Log In.</h2>
+        isLoggedIn?<h2>Hello {name}! You have {messageCount?messageCount:0} message unread!.</h2>: <h2>Hello User! Please Log In.</h2>
     }
     
     </>
