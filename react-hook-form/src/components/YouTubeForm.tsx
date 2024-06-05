e6375cf26d1eb370be36069a4a1e8aa3c407794d
NA-Asraful-Khan <soyonshikhor@gmail.com>
2024-06-05 11:22:43 +0600

    RHF form submit

--- react-hook-form/src/components/YouTubeForm.tsx
+++ react-hook-form/src/components/YouTubeForm.tsx
@@ -1,20 +1,33 @@
 import { useForm } from "react-hook-form"
 import {DevTool}from "@hookform/devtools"
 
+type FormValues ={
+  username:string,
+  email:string,
+  channel:string
+}
+
 
 export const YouTubeForm = () => {
-  const form = useForm()
-  const {register,control}=form
+  let renderCount =0
+  const form = useForm<FormValues>()
+  const {register,control,handleSubmit}=form
+
+  const submitForm=(data:FormValues)=>{
+    console.log('Form Submitted', data)
+  }
+
+  renderCount++
   return (
     <div>
-      <h1>YouTube Form</h1>
+      <h1>YouTube Form ({renderCount})</h1>
 
-      <form>
+      <form onSubmit={handleSubmit(submitForm)}>
         <label htmlFor="username">Username</label>
         <input
          type="text" 
          id="username" 
-         {...register('username')}/>
+         {...register("username")}/>
 
         <label htmlFor="email">E-mail</label>
         <input type="email" id="email" {...register('email')} />
