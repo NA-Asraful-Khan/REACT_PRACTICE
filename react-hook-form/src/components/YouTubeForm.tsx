fb5d331397e62f302b10df3abd841decb2aa0a1c
NA-Asraful-Khan <soyonshikhor@gmail.com>
2024-06-08 09:51:34 +0600

    RHF handeling object and array

--- react-hook-form/src/components/YouTubeForm.tsx
+++ react-hook-form/src/components/YouTubeForm.tsx
@@ -4,19 +4,42 @@ import { DevTool } from "@hookform/devtools"
 type FormValues = {
   username: string,
   email: string,
-  channel: string
+  channel: string,
+  social: {
+    twitter: string,
+    facebook: string
+  },
+  phoneNumbers: string[];
 }
 
 
 export const YouTubeForm = () => {
   let renderCount = 0
-  const form = useForm<FormValues>()
+  const form = useForm<FormValues>({
+    defaultValues: async () => {
+      const response = await fetch("https://jsonplaceholder.typicode.com/users/1")
+      const data = await response.json()
+      return {
+        username: data.name,
+        email: data.email,
+        channel: "",
+        social: {
+          twitter: "",
+          facebook: ""
+        },
+        phoneNumbers: [
+          "", ""
+        ],
+      }
+    }
+  })
   const { register, control, handleSubmit, formState } = form
   const { errors } = formState
 
   const submitForm = (data: FormValues) => {
     console.log('Form Submitted', data)
   }
+  
 
   renderCount++
   return (
@@ -43,17 +66,17 @@ export const YouTubeForm = () => {
               value: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
               message: 'Invalid Email Format'
             },
-            validate:{
-              notAdmin:(fieldValue)=>{
-                return(fieldValue !=="admin@example.com" || "Enter a different Email Address")
+            validate: {
+              notAdmin: (fieldValue) => {
+                return (fieldValue !== "admin@example.com" || "Enter a different Email Address")
               },
-              notBlackListed:(fieldValue)=>{
-                return(
-                  !fieldValue.endsWith("baddomain.com")||"This Domain is not supported"
+              notBlackListed: (fieldValue) => {
+                return (
+                  !fieldValue.endsWith("baddomain.com") || "This Domain is not supported"
                 )
               }
             },
-            
+
           })} />
           <p className="error">{errors.email?.message}</p>
         </div>
@@ -65,9 +88,36 @@ export const YouTubeForm = () => {
           })} />
           <p className="error">{errors.channel?.message}</p>
         </div>
+
+        <div className="form-control">
+          <label htmlFor="twitter">Twitter</label>
+          <input type="text" id="twitter" {...register('social.twitter', {
+            required: 'This field is erquired'
+          })} />
+
+          <p className="error">{errors.social?.twitter?.message}</p>
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="facebook">facebook</label>
+          <input type="text" id="facebook" {...register('social.facebook')} />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="primariPhone">Primary Number</label>
+          <input type="text" id="primariPhone" {...register('phoneNumbers.0')} />
+        </div>
+
+        <div className="form-control">
+          <label htmlFor="secondaryPhone">Secondary Number</label>
+          <input type="text" id="secondaryPhone" {...register('phoneNumbers.1', {
+            required: 'Phone Number is erquired'
+          })} />
+        </div>
+        <p className="error">{errors.phoneNumbers?.[1]?.message}</p>
         <button>Submit</button>
       </form>
-
+  
       <DevTool control={control} />
     </div>
   )
